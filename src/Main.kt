baa37cbd9c26ecd696e0fecf3ba950bab5b4347e
Bashayer770 <Beshow95@gmail.com>
2025-02-18 20:11:44 +0300

    Bashayer

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,32 @@
 fun main() {
-    println("Hello World!")
+var admin = UserRole.ADMIN
+    var editor = UserRole.EDITOR
+    var viewer = UserRole.VIEWER
+
+var res = permission(editor,2)
+println(res)
+
+    for(roles in UserRole.entries){
+        println(roles.name)
+    }
+
+
 }
+
+enum class UserRole(){
+    ADMIN ,
+    EDITOR,
+    VIEWER
+}
+
+
+fun permission(userRole: UserRole,  required:Int): Boolean{
+val accessLevel = when(userRole){
+    UserRole.ADMIN -> 3
+    UserRole.EDITOR ->2
+    UserRole.VIEWER -> 1
+
+}
+    return accessLevel >= required
+}
+
